9979dfd66a3d8ee510117567826434f810cf61e2
ayushpathakz27 <115604561+ayushpathakz27@users.noreply.github.com>
2023-12-09 19:42:28 +0530

    Add files via upload

---
+++ SumofArray.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+using namespace std;
+
+int Sum(int arr[], int n){
+    //base case
+    if(n == 0){
+        return 0;
+    }
+
+    if(n==1){
+        return arr[0];
+    }
+    int remPart = Sum(arr+1, n-1);
+    int sum = arr[0] + remPart;
+    
+
+    return sum;
+    
+
+    //recursive call
+    
+    
+
+    
+}
+
+int main(){
+    int arr[100], n;
+
+    cout<<"Enter size of array: ";
+    cin>>n;
+
+    cout<<"Enter elements of array: "<<endl;
+    for(int i=0; i<n; i++){
+        cin>>arr[i];
+    }
+
+    int sum = Sum(arr, n);
+
+    cout<<"Sum of all elements is array: "<<sum<<endl;
+
+    return 0;
+}
